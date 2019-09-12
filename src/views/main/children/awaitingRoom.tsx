c25df992cdfafc6c3c3cb4797b215c5d9d18313c
2565497683zxc <L18810857853@163.com>
2019-09-12 12:09:03 +0800

    'gen'

--- src/views/main/children/awaitingRoom.tsx
+++ src/views/main/children/awaitingRoom.tsx
@@ -1,13 +1,99 @@
 import * as React from 'react'
-
-class awaitingRoom extends React.Component{
-       render(){
-           return (
-                <div>
-                      待批班级
+import { Table } from 'antd';
+import { inject, observer } from 'mobx-react';
+import { WrappedFormUtils } from 'antd/lib/form/Form';
+import { runInThisContext } from 'vm';
+const columns = [
+    {
+        title: '班级名',
+        dataIndex: 'name',
+    },
+    {
+        title: '课程名称',
+        dataIndex: 'Coursetitle',
+    },
+    {
+        title: '阅卷状态',
+        dataIndex: 'Markingstate',
+    },
+    {
+        title: '课程名称',
+        dataIndex: 'Coursetitles',
+    },
+    {
+        title: '成材率',
+        dataIndex: 'yield',
+    },
+    {
+        title: '操作',
+        dataIndex: 'operation',
+    },
+];
+const data = [
+    {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+    },
+    {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+    },
+    {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park',
+    },
+];
+interface Props {
+    form: WrappedFormUtils,
+    graded?: any
+}
+@observer
+@inject('graded')
+class awaitingRoom extends React.Component<Props>{
+    state = {
+        data: []
+    }
+    async componentDidMount() {
+        const result = await this.props.graded.grade()
+        console.log(result);
+        const data = result.data.map((item: any, index: any) => {
+            return {
+                key: index,
+                name: item.grade_name,
+                Coursetitle: item.subject_text,
+                Markingstate: '',
+                Coursetitles: item.subject_text,
+                yield: item.room_text,
+                operation: <a href="#">批卷</a>,
+            }
+        })
+        this.setState({
+            data: data
+        })
+    }
+    render() {
+        return (
+            <div className="box_awaiting">
+                <div className="box_awaiting_top">
+                    <p>
+                        待批班级
+                        </p>
                 </div>
-           )
-       }
+                <div className="box_awaiting_con">
+                    <div>
+                        <Table columns={columns} dataSource={this.state.data} size="middle" />
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
 }
 
 
--- src/views/main/children/classManagement.tsx
+++ src/views/main/children/classManagement.tsx
@@ -3,6 +3,7 @@ import { Table, Modal, Button, Input } from 'antd';
 import { inject, observer } from 'mobx-react';
 import { WrappedFormUtils } from 'antd/lib/form/Form';
 import './css/index.css';
+import { runInThisContext } from 'vm';
 const columns = [
      {
           title: '班级名',
@@ -25,22 +26,28 @@ interface Props {
      form: WrappedFormUtils,
      Class_managementd?: any,
      graded?: any,
-     deleteads?:any,
-     addClassdx?:any
+     deleteads?: any,
+     addClassdx?: any
 }
 @observer
-@inject('Class_managementd', 'graded','deleteads','addClassdx')
+@inject('Class_managementd', 'graded', 'deleteads', 'addClassdx')
 class classManagement extends React.Component<Props> {
      state = {
           loading: false,
           visible: false,
-          data:[],
-          int2:'',
-          int1:'',
-          int3:'',
-          room_id:'',
-          subject_id:"",
-          flag:false
+          data: [],
+          int2: '',
+          int1: '',
+          int3: '',
+          room_id: '',
+          subject_id: "",
+          flag: false,
+          curriculum:'',
+          grade_name:'',
+          curriculums:'',
+          Classroom:'',
+          int4:'',
+          Classrooms:''
 
      };
      showModal = () => {
@@ -53,15 +60,15 @@ class classManagement extends React.Component<Props> {
           this.setState({ loading: true });
           setTimeout(() => {
                this.setState({ loading: false, visible: false });
-          },0);
+          }, 0);
           const int1 = this.state.int1;
           const int2 = this.state.int2;
           const int3 = this.state.int3;
-          console.log(int1,int2,int3)
-          this.pawnd({grade_name:int1,room_id:this.state.room_id,subject_id:this.state.subject_id})
+          console.log(int1, int2, int3)
+          this.pawnd({ grade_name: int1, room_id: this.state.room_id, subject_id: this.state.subject_id })
           this.pase()
      };
-     async pawnd(params:any){
+     async pawnd(params: any) {
           const result = await this.props.addClassdx.addClassd(params)
           console.log(result);
 
@@ -69,59 +76,71 @@ class classManagement extends React.Component<Props> {
      async componentDidMount() {
           this.pase()
      }
-     async pase(){
+     async pase() {
           const result = await this.props.graded.grade();
           console.log(result, "+++++++++");
           const classMangage = result.data.map((item: any, index: any) => {
                return {
                     key: index,
                     className: item.grade_name,
-                    curriculum:item.subject_text,
+                    curriculum: item.subject_text,
                     Classroom: item.room_text,
-                    operation:<p><a href="#" onClick={this.xiug}>修改</a>|<a href="#" onClick={this.deletes.bind(this,item)}>删除</a></p>
+                    operation: <p><a href="#" onClick={this.xiug.bind(this,item)}>修改</a>|<a href="#" onClick={this.deletes.bind(this, item)}>删除</a></p>
                }
           })
           this.setState({
                data: classMangage,
-               room_id:result.data[0].room_id,
-               subject_id:result.data[0].subject_id
+               room_id: result.data[0].room_id,
+               subject_id: result.data[0].subject_id,
           })
      }
-     xiug=()=>{
+     xiug = (item:any) => {
+          console.log(item)
+          this.setState({
+               grade_name:item.grade_name
+          })
+          
           this.setState({
                flag: true,
+               
           });
+
+
      }
-     quxiao=()=>{
+     quxiao = () => {
           this.setState({
                flag: false,
           });
      }
-     deletes=(item:any)=>{
+     deletes = (item: any) => {
           console.log(item.grade_id)
-          this.delde({grade_id:item.grade_id})
+          this.delde({ grade_id: item.grade_id })
           this.pase()
      }
-     async delde(grade_id:any){
+     async delde(grade_id: any) {
           await this.props.deleteads.deletea(grade_id);
      }
      handleCancel = () => {
           this.setState({ visible: false });
      };
-     int1=(e:any)=>{
+     int1 = (e: any) => {
           this.setState({
-               int1:e.target.value
+               int1: e.target.value
           })
      }
-     int2=(e:any)=>{
+     int2 = (e: any) => {
           this.setState({
-               int2:e.target.value
+               int2: e.target.value
           })
      }
-     int3=(e:any)=>{
+     int3 = (e: any) => {
           this.setState({
-               int3:e.target.value
+               int3: e.target.value
           })
+     }
+     conmit=()=>{
+         
+          
      }
      render() {
           const { visible, loading } = this.state;
@@ -151,11 +170,11 @@ class classManagement extends React.Component<Props> {
                                                   </Button>,
                                              ]} className="Modals">
                                              <p>*班级名:</p>
-                                             <Input placeholder="班级名" onBlur={this.int1}/>
+                                             <Input placeholder="班级名" onBlur={this.int1} />
                                              <p>*教室名:</p>
-                                             <Input placeholder="请选择教室号" onBlur={this.int2}/>
+                                             <Input placeholder="请选择教室号" onBlur={this.int2} />
                                              <p>*课程名:</p>
-                                             <Input placeholder="课程名" onBlur={this.int3}/>
+                                             <Input placeholder="课程名" onBlur={this.int3} />
                                         </Modal>
                                    </div>
                               </div>
@@ -167,34 +186,55 @@ class classManagement extends React.Component<Props> {
                          </div>
                     </div>
                     <div>
-                         {this.state.flag?
+                         {this.state.flag ?
                               <div className="mask">
-                                   <div className="mask-div">
-                                   <p>
-                                        <input className="inp" type="text" placeholder=" 班级名" />
-                                   </p>
-                                   <p>
-                                        <select name="" id="classNumber">
-                                             <option value="">请选择教室号</option>
-                                             <option>haha</option>
-                                        </select>
-                                   </p>
-                                   <p>
-                                        <select name="" id="curriculumName">
-                                             <option value="">课程名</option>
-                                   
-                                                  <option>haha</option>
-                                             
-                                        </select>
-                                   </p>
-                                   <p id="mask-btn">
-                                        <button id="mask-cancel"  onClick={this.quxiao}>取消</button><button id="mask-submit">提交</button>
-                                   </p>
 
+                                   <div className="mask-div">
+
+                                        <div>
+                                             <p>
+                                                  
+                                                       <input className="inp" type="text" placeholder=" 班级名" value={this.state.grade_name}/>  
+                                                  
+                                             </p>
+                                             <p>
+                                                  <select name="" id="classNumber">
+                                                       <option value="">请选择教室号</option>
+                                                       {
+                                                            this.state.data.map((item: any, index: any) => {
+                                                      
+                                                                 return <option key={index}>{item.Classroom}</option>
+
+                                                            })
+                                                       }
+
+
+                                                  </select>
+                                             </p>
+                                             <p>
+                                                  <select name="" id="curriculumName">
+                                                       <option value="">课程名</option>
+                                                       {
+                                                            this.state.data.map((item: any, index: any) => {
+                                                         
+                                                                 return <option key={index}>{item.curriculum}</option>
+
+                                                            })
+                                                       }
+
+
+                                                  </select>
+                                             </p>
+                                             <p id="mask-btn">
+                                                  <button id="mask-cancel" onClick={this.quxiao}>取消</button><button id="mask-submit" onClick={this.conmit}>提交</button>
+                                             </p>
+                                        </div>
                                    </div>
-                              </div>:''}
-                         
-                              
+
+
+                              </div> : ''}
+
+
                     </div>
                </div>
           )
